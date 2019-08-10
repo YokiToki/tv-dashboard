e18dd01a8887c79a1e91dd150babb327c512b898
tamat <libastral.so@yandex.ru>
2019-08-11 02:19:41 +0700

    Updates

---
+++ precache-manifest.038cc1c60f083db6ab244d8ca913677e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92fd44b87fd72150c6dfcbe9297dbad1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5f0e975d05bb286552d",
+    "url": "./static/css/main.0936b586.chunk.css"
+  },
+  {
+    "revision": "457ea7f748dadf5b232c",
+    "url": "./static/js/2.7a092657.chunk.js"
+  },
+  {
+    "revision": "e5f0e975d05bb286552d",
+    "url": "./static/js/main.fd85dff5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "85212ff61f1e3b801517a892a16c7828",
+    "url": "./static/media/forkplayer.85212ff6.svg"
+  }
+]);
